fd27f2abe6142c7dcd71dc2abc50dd91384f189b
Farshad Nabizadeh <farshadnabizadeh.crux@gmail.com>
2023-05-29 02:27:55 -0700

    router-push

--- pages/NextLink/index.js
+++ pages/NextLink/index.js
@@ -6,6 +6,12 @@ const index = () => {
     const LinkProcess = () => {
         router.push('/products/12/21')
     }
+    const BlogProcess = () => {
+        router.push({
+            pathname: '/blog/[userid]',
+            query: { userid: 12 }
+        })
+    }
     return (
         <div className='w-full min-h-[100vh] flex justify-center'>
             <div>
@@ -25,6 +31,11 @@ const index = () => {
                 <div>
                     <button className='p-5 bg-slate-900 text-lg text-white rounded-md my-12' onClick={(e) => LinkProcess()}>
                         go to products
+                    </button>
+                </div>
+                <div>
+                    <button className='p-5 bg-slate-900 text-lg text-white rounded-md my-12' onClick={(e) => BlogProcess()}>
+                        go to blog
                     </button>
                 </div>
             </div>
