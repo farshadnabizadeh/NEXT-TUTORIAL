681770b04d35d62dcb7c196f6aa1cf69b7988dc3
Farshad Nabizadeh <farshadnabizadeh.crux@gmail.com>
2023-05-29 02:13:09 -0700

    router.push

--- pages/NextLink/index.js
+++ pages/NextLink/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Link from "next/link";
+import { useRouter } from "next/router";
 const index = () => {
+    const router = useRouter();
+    const LinkProcess = () => {
+        router.push('/products/12/21')
+    }
     return (
         <div className='w-full min-h-[100vh] flex justify-center'>
             <div>
@@ -16,6 +21,11 @@ const index = () => {
                         pathname: '/products/[...slug]',
                         query: { slug: '/1/12/8080?DATA=NEXT.JS' }
                     }}>CLICK HERE . . . </Link>
+                </div>
+                <div>
+                    <button className='p-5 bg-slate-900 text-lg text-white rounded-md my-12' onClick={(e) => LinkProcess()}>
+                        go to products
+                    </button>
                 </div>
             </div>
         </div>
