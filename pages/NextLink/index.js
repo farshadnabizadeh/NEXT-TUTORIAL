2fcb80551877da3f8dae190e9e6564e1ce259540
Farshad Nabizadeh <farshadnabizadeh.crux@gmail.com>
2023-05-29 02:02:43 -0700

    next-tutorial

--- pages/NextLink/index.js
+++ pages/NextLink/index.js
@@ -4,7 +4,19 @@ const index = () => {
     return (
         <div className='w-full min-h-[100vh] flex justify-center'>
             <div>
-                <Link href='/products/1/80?DATA=THIS IS TEST'>CLICK HERE . . .</Link>
+                <div><Link href='/products/1/80?DATA=THIS IS TEST'>CLICK HERE . . .</Link></div>
+                <div>
+                    <Link href={{
+                        pathname: '/products/[userid]',
+                        query: { userid: '80' }
+                    }}>CLICK HERE . . . </Link>
+                </div>
+                <div>
+                    <Link href={{
+                        pathname: '/products/[...slug]',
+                        query: { slug: '/1/12/8080?DATA=NEXT.JS' }
+                    }}>CLICK HERE . . . </Link>
+                </div>
             </div>
         </div>
     );
